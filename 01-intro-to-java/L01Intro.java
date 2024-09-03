cab41e5cb6ce6353a5c3b543486ac41c212e2136
kunxin-chor <chorkunxin@yahoo.com>
2024-09-03 02:07:49 +0000

    Added primitive variables

--- 01-intro-to-java/L01Intro.java
+++ 01-intro-to-java/L01Intro.java
@@ -3,5 +3,34 @@ public class L01Intro {
     // all Java programs
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // what are data types that a programming langauge support?
+        // is the langauge loosely-type or strongly-typed? (Java is the latter)
+        // when we define a variable, we must specify its data type and that cannot be changed
+        int secretOfLife = 42;  // +2 billion to -2 billion
+        // secretOfLife = "forty two"; // illegal! secretOfLife can only store integers
+        short moneyInBalance = 3200;   
+
+        // decimal numbers
+        double gstPercentage = 0.07;
+        float f = 1.0f;  // floating point values (aka float) have a `f` at the back
+
+        // Strings (must be defined with double quotes)
+        // String are objects in JavaScript
+        String sentence = "She sells seashell at the seashore";
+        String word = "hello";
+
+        // char
+        // is represent one character
+        char alpha = 'A';  
+
+        // boolean - true/false
+        boolean isRaining = false;
+
+        System.out.println("Secret of Life =" + secretOfLife);
+        System.out.println(isRaining);
+
+
+  
     }
 }
