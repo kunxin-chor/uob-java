20a42377a4abb0edcb3737f5de042f57acd40b00
kunxin-chor <chorkunxin@yahoo.com>
2024-09-03 01:52:30 +0000

    Added hello world

---
+++ 01-intro-to-java/L01Intro.java
@@ -0,0 +1,7 @@
+public class L01Intro {
+    // the main method is the entry point of
+    // all Java programs
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # uob-java
+
+## How to Compile and Run a Java program
+1. Launch terminal
+2. Change the working directory to the directory with `.java` fil
+3. `javac <java file>.java`
+4. `java <java file>`
