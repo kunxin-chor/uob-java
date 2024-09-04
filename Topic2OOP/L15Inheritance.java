03fa0671ef63891edc412e9f4cea50bd5b75998e
kunxin-chor <chorkunxin@yahoo.com>
2024-09-04 07:22:43 +0000

    Added overriding

--- Topic2OOP/L15Inheritance.java
+++ Topic2OOP/L15Inheritance.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 
 public class L15Inheritance {
     public static void main(String args[]) {
-        SavingAccount account = new SavingAccount();
-        System.out.println(account.getFirstName());
-        System.out.println(account.getLastName());
+        SavingAccount account = new SavingAccount("Jon", "Snow", 1000, 0.05);
+        System.out.println(account); // when we try to print object, Java will automatically call `toString()` and use its return value
+
+
 
         // when we define a class we are defining a new data type
          // This arraylist can only store instances of SavingAccount
@@ -17,48 +18,17 @@ public class L15Inheritance {
         // what if we want to be able to store checking accounts and saving accounts together?
         // create an arraylist that stores the parent class of checking account and saving account
         ArrayList<BankAccount> allAccounts = new ArrayList<BankAccount>();
-        allAccounts.add(new SavingAccount());
-        allAccounts.add(new CheckingAccount());
+        allAccounts.add(new SavingAccount("Jon", "Snow", 1000, 0.0025));
+        allAccounts.add(new CheckingAccount("Tyrion", "Lannister", 2000.0, 250.0));
 
+        for (BankAccount a : allAccounts) {
+            System.out.println(a);
+        }
 
     }
 }
 
-// do inheritance with extends
-// the child class inherits all the properties and methods of the parent class
-// syntax:
-// class <child class> extends <parent class> { }
-class SavingAccount extends BankAccount {
-    private double interestRate;
 
-    // set the default interestRate to 0.001
-    public SavingAccount() {
-        // call the parent's default constructor
-        super();  // `super` here refers to the parent class
-        interestRate = 0.001;
-    }
-
-    public SavingAccount(String firstName, String lastName, double balance, double interestRate) {
-        // call the parent's the overloaded class
-        super(firstName, lastName, balance);
-        this.interestRate = interestRate;
-    }
-
-    public double getInterestRate() {
-        return this.interestRate;
-    }
-
-    public void setInterestRate(double interestRate) {
-        this.interestRate = interestRate;
-    }
-
-    public String getInfo() {
-        // return a string that shows the balance, the interest rate and the first name and last name
-        // note: we need use getFirstName() because firstName is private in the parent class
-        // whereas lastName and balance are protected in the parent class.
-        return getFirstName() + " " + lastName + " have balance of " + balance + " and interest rate is " + interestRate;
-    }
-}
 
 // Demonstratio of grandchild inheritance
 // usually it's a bad practice to have a very deep inheritance chain
@@ -77,9 +47,7 @@ class SavingAccount extends BankAccount {
 //     }
 // }
 
-class CheckingAccount extends BankAccount {
-    private double overDraftLimit;
-}
+
 
 // How to support saving accounts and checking account?
 // saving accounts have interest rate, but no overdraft limit
@@ -126,8 +94,64 @@ class BankAccount {
         this.balance = balance;
     }
 
+    public String toString() {
+        return firstName + " " + lastName + "," + balance;
+    }
     
     
 }
 
 
+// do inheritance with extends
+// the child class inherits all the properties and methods of the parent class
+// syntax:
+// class <child class> extends <parent class> { }
+class SavingAccount extends BankAccount {
+    private double interestRate;
+
+    // set the default interestRate to 0.001
+    public SavingAccount() {
+        // call the parent's default constructor
+        super();  // `super` here refers to the parent class
+        interestRate = 0.001;
+    }
+
+    public SavingAccount(String firstName, String lastName, double balance, double interestRate) {
+        // call the parent's the overloaded class
+        super(firstName, lastName, balance);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return this.interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public String getInfo() {
+        // return a string that shows the balance, the interest rate and the first name and last name
+        // note: we need use getFirstName() because firstName is private in the parent class
+        // whereas lastName and balance are protected in the parent class.
+        return getFirstName() + " " + lastName + " have balance of " + balance + " and interest rate is " + interestRate;
+    }
+
+    // the child's class toString will override the parent's class toString
+    public String toString() {
+        return getFirstName() + " " + lastName + ", balance=" + balance + " intrestRate=" + interestRate;
+    }
+}
+
+class CheckingAccount extends BankAccount {
+    private double overDraftLimit;
+    public CheckingAccount(String firstName, String lastName, double balance, double overdraftLimit) {
+        super(firstName, lastName, balance);
+        this.overDraftLimit = overdraftLimit;
+    }
+
+    public String toString() {
+        return super.toString() +  ", overdraftLimit=" + overDraftLimit;
+    }
+
+}
