729f3e7be1a1d1b97b0aa1460ba8ffbb20e0cce4
kunxin-chor <chorkunxin@yahoo.com>
2024-09-04 07:26:11 +0000

    demostrated how to have each class in its own file

---
+++ Topic3SimpleBankAccountApp/BankAccount.java
@@ -0,0 +1,8 @@
+package Topic3SimpleBankAccountApp;
+
+// if the class is public, IT MUST BE ITS OWN FILE
+// (however the inverse is not true)
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+}
---
+++ Topic3SimpleBankAccountApp/Main.java
@@ -0,0 +1,7 @@
+package Topic3SimpleBankAccountApp;
+
+public class Main {
+    public static void main() {
+        System.out.println("Hello!");
+    }
+}
